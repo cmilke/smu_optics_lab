eac9cdd8f2c9a66cce6274ebc02b9e72838a7cf6
Milke <cmilke@smu.edu>
2017-11-03 19:31:09 -0500

    it makes the lil red light blink!

--- LOCx2_SetReg.py
+++ LOCx2_SetReg.py
@@ -1,16 +1,17 @@
 import visa
+import pyvisa
 import time
 import testing_utils
 
 
 
-com10_visa_string = 'COM10'
-adc_type = 0x0001 #FIXME: this is an enum with 4 options
-ref_clk_delay = 0 #FIXME: I think this is also an enum...
-delay_array = [0x17,0x17,0x17,0x17]
+com10_visa_string = 'COM8'
+adc_enum = {'NevisADC':0, 'ADS5272':1, 'ADS5294':2, 'Testmode':3}
+ref_clk_delay = 0
+delay_array = [17,17,17,17]
 write_register_array = [0x0,0x2,0x4,0x6]
 read_register_array = [0x1,0x3,0x5,0x7]
-init_command = [0x5A,0x02,0x60]
+init_command = [0x5A,0x2,0x60]
 
 
 
@@ -18,7 +19,8 @@ init_command = [0x5A,0x02,0x60]
 #The ordering flips directions multiple times.
 def locx2_regs_gen(adc_type, ref_clk_delay, delay_array):
     delay_binary = []
-    for i in range(4): delay_binary.append( testing_utils.to_bits(delay_array[i],8,True) )
+    for i in range(4):
+        delay_binary.append( testing_utils.to_bits(delay_array[i],8,True) )
 
     delay_piece0 = delay_binary[0][0:4]
     delay_piece1 = delay_binary[0][4]
@@ -34,10 +36,13 @@ def locx2_regs_gen(adc_type, ref_clk_delay, delay_array):
     command_string1 = adc_type_binary + ref_clk_delay_value_binary + delay_piece0
     command_string2 = delay_piece1 + delay_piece2 + delay_piece3
     command_string3 = delay_piece4 + delay_piece5
-
+    print(adc_type_binary+' '+ref_clk_delay_value_binary)
+    print(delay_piece0+' '+delay_piece1+' '+delay_piece2)
+    print(delay_piece3+' '+delay_piece4+' '+delay_piece5)
     command1 = testing_utils.to_integer(command_string1,True)
     command2 = testing_utils.to_integer(command_string2,True)
     command3 = testing_utils.to_integer(command_string3,True)
+    print("COMMANDS: "+format(command1,'02x')+' '+format(command2,'02x')+' '+format(command3,'02x'))
 
     generated_bytes = [13,command1,command2,command3]
     return generated_bytes
@@ -47,20 +52,26 @@ def locx2_regs_gen(adc_type, ref_clk_delay, delay_array):
 def usb_12c_wr(visa_resource_name, write_mode, read_mode, command):
     com10_instrument = visa.ResourceManager().open_resource(visa_resource_name,
                                                             open_timeout=10000, #10 seconds
-                                                            resource_pyclass=pyvisa.resources.SerialInstrument,
                                                             baud_rate=9600,
                                                             data_bits=8,
+                                                            resource_pyclass=pyvisa.resources.SerialInstrument,
                                                             parity=pyvisa.constants.Parity.none,
                                                             stop_bits = pyvisa.constants.StopBits.one)
 
     if write_mode:
-        com10_instrument.write( chr(write_string) )
-    time.sleep(0.5) #500 milliseconds
+        print(command)
+        command_string = testing_utils.array_to_asciistring(command)
+        print(command_string)
+        print("SENDING STRING")
+        com10_instrument.write_ascii_values(command_string)
+        print()
+    time.sleep(.5) #500 milliseconds
     data_read=-1
     if read_mode:
         data_read = com10_instrument.read()
 
     #TODO: display read string and bytes read
+    com10_instrument.close()
     return data_read
 
 
@@ -70,22 +81,23 @@ def display_test_results(test_passed):
     print('Did test pass? ' + str(test_passed) )
 
 
-
+adc_name = 'NevisADC'
+adc_type = adc_enum[adc_name]
 generated_byte_array = locx2_regs_gen(adc_type, ref_clk_delay, delay_array)
-
 usb_12c_wr(com10_visa_string, True, False, init_command)
-for register_number in range( size(write_register_array) ):
+print("Filling Registers\n\n")
+for register_number in range( len(write_register_array) ):
     write_register = write_register_array[register_number]
     generated_byte = generated_byte_array[register_number]
     write_command = [0x53, write_register, generated_byte]
     usb_12c_wr(com10_visa_string, True, False, write_command)
-    #TODO: print write_command out
-
+print("\n\nDONE")
+exit(0)
 
 usb_12c_wr(com10_visa_string, True, False, init_command)
 values_read = []
 read_comparison_failed = False
-for register_number in range( size(read_register_array) ):
+for register_number in range( len(read_register_array) ):
     read_register = read_register_array[register_number]
     generated_byte = generated_byte_array[register_number]
     read_command = [0x53, read_register]
@@ -97,7 +109,7 @@ for register_number in range( size(read_register_array) ):
     read_comparison_failed = comparison_byte != generated_byte
     if read_comparison_failed: break
 
-if read_comparison_failed
+if read_comparison_failed:
     display_test_results(False)
 
 #print generated_byte_array
--- testing_utils.py
+++ testing_utils.py
@@ -1,6 +1,6 @@
 def to_bits(value, number_of_bits, reverse):
     format_string = '0' + str(number_of_bits) + 'b'
-    bit_string = format(value,format_string)[::-1]
+    bit_string = format(value,format_string)
     if reverse:
         return bit_string[::-1]
     else:
@@ -20,4 +20,11 @@ def array_to_bitstring(array, bits_per_element):
     for value in array:
         bitstring += to_bits(value,bits_per_element,False)
     return bitstring
+    
+    
+def array_to_asciistring(array):
+    asciistring = ''
+    for value in array:
+        asciistring += chr(value)
+    return asciistring
 
